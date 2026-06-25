1284efb8de73b85cf3403315f8812ab09a491da1
Yurii Nikogosyan <ura.nikogosuan@gmail.com>
2026-06-25 19:04:50 +0500

    Удалил sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-python3 -c "import PySide6" >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "PySide6 is not installed."
-  echo "Run: python3 -m pip install -r requirements.txt"
-  exit 1
-fi
-python3 main.py
